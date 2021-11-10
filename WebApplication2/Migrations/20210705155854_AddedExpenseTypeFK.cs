e094ae95cc5c775902bdcc1c7c4c07d668238404
Kenan Brodlija <kenanbrodlija@gmail.com>
2021-11-10 21:11:35 +0100

    Commit

---
+++ WebApplication2/Migrations/20210705155854_AddedExpenseTypeFK.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication2.Migrations
+{
+    public partial class AddedExpenseTypeFK : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExpenseTypeId",
+                table: "Expenses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_ExpenseTypes_ExpenseTypeId",
+                table: "Expenses",
+                column: "ExpenseTypeId",
+                principalTable: "ExpenseTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+            migrationBuilder.DropIndex(
+                name: "IX_Expenses_ExpneseTypeId",
+                table: "Expenses");
+
+            migrationBuilder.DropColumn(
+                name: "ExpneseTypeId",
+                table: "Expenses");
+        }
+    }
+}
---
+++ WebApplication2/Migrations/20210707140612_AddForeignKeyExpenseTypeId.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication2.Migrations
+{
+    public partial class AddForeignKeyExpenseTypeId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_ExpenseTypes_ExpenseTypeId",
+                table: "Expenses");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_ExpenseTypes_ExpenseTypeId",
+                table: "Expenses",
+                column: "ExpenseTypeId",
+                principalTable: "ExpenseTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_ExpenseTypes_ExpenseTypeId",
+                table: "Expenses");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_ExpenseTypes_ExpenseTypeId",
+                table: "Expenses",
+                column: "ExpenseTypeId",
+                principalTable: "ExpenseTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
